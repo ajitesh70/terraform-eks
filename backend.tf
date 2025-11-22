0f6456a8b2edbb563f2b1bae6c9d26d9072f5a08
Ubuntu <ubuntu@ip-10-0-1-172.ap-south-1.compute.internal>
2025-11-22 13:20:10 +0000

    added backend automation folder and removed old backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ajitesh-terraform-backend-bucket"
-    key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
---
+++ backend/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket        = "ajitesh-tf-backend-${random_string.suffix.id}"
+  force_destroy = true
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "bucket" {
+  value = aws_s3_bucket.tf_state.bucket
+}
+
+output "dynamodb_table" {
+  value = aws_dynamodb_table.tf_lock.name
+}
