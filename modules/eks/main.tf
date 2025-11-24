957903556ed834a80e5dd2ae8c143fb5da6c498e
Ubuntu <ubuntu@ip-172-31-15-232.ap-south-1.compute.internal>
2025-11-24 12:45:21 +0000

    Fix: add NAT + private subnet routing for EKS node group

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -44,6 +44,35 @@ resource "aws_route_table_association" "assoc" {
 }
 
 #############################################
+# NAT GATEWAY FOR PRIVATE SUBNETS
+#############################################
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route" "private_route" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+#############################################
 # IAM ROLES FOR EKS
 #############################################
 
