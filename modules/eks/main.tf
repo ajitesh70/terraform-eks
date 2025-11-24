f3e57770d889f225e41ae34f844c899cdf775417
Ubuntu <ubuntu@ip-172-31-15-232.ap-south-1.compute.internal>
2025-11-24 11:40:56 +0000

    Fix EKS subnets (use 2 AZs)

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,105 +1,13 @@
-
-##########################
-# Cheapest VPC (Public Only)
-##########################
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-}
-
 data "aws_availability_zones" "az" {}
 
 resource "aws_subnet" "public" {
+  count                   = 2
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = data.aws_availability_zones.az.names[count.index]
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.az.names[0]
-}
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_route" "route" {
-  route_table_id         = aws_route_table.rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.rt.id
-}
-
-##########################
-# EKS Cluster + Node Group
-##########################
-resource "aws_iam_role" "cluster_role" {
-  name = "eks-cluster-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  role       = aws_iam_role.cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_eks_cluster" "cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.cluster_role.arn
-  vpc_config {
-    subnet_ids = [aws_subnet.public.id]
-  }
-}
-
-resource "aws_iam_role" "node_role" {
-  name = "eks-node-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_pol1" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-resource "aws_iam_role_policy_attachment" "node_pol2" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-resource "aws_iam_role_policy_attachment" "node_pol3" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "cheap-ng"
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = [aws_subnet.public.id]
-
-  instance_types = ["t3.micro"]
-
-  scaling_config {
-    desired_size = 1
-    min_size     = 1
-    max_size     = 1
+  tags = {
+    Name = "public-${count.index}"
   }
 }
